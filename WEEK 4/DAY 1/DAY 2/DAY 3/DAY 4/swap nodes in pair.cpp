a7809327c5863c407bebdc37a68009bc91d4248e
Sanyog Rana <sanyograna0333@gmail.com>
2026-06-25 23:12:20 +0530

    day 4 solution uploaded

---
+++ WEEK 4/DAY 1/DAY 2/DAY 3/DAY 4/swap nodes in pair.cpp
@@ -0,0 +1,62 @@
+
+class Solution {
+public:
+
+    void reverse(ListNode* head , int times){
+        ListNode* curr = head;
+        ListNode* prev = 0;
+        while(times--){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next; 
+        }
+        return;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* left = head;
+        ListNode* right;
+        ListNode* prevleft = 0;
+        ListNode* res = 0;
+
+        int s = 2;
+        if(head == 0){
+                return 0;
+            }
+
+        while(true)
+        {
+            right = left;
+
+            for(int i=0; i<(s-1); i++){
+                if(right==0){
+                  break;
+                }
+                right = right->next;
+            }
+            if(right){
+                ListNode* nextleft = right->next;
+                reverse(left,s);
+                if(prevleft){
+                    prevleft->next= right;
+                }
+                prevleft = left;
+                if(res==0){
+                    res = right;
+                }
+                left = nextleft;
+            }
+            else{
+                if(prevleft){
+                    prevleft->next = left;
+                }
+                if(res ==0){
+                    res = left;
+                }
+                break;
+            }
+        }
+        return res;
+    }
+};
